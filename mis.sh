1efbf29a5cd2a290b9a143fcd6de78091a68ecc3
ian shirley <ian shirley>
2016-12-08 16:08:57 -0600

    File structure created

---
+++ mis.sh
@@ -0,0 +1,10 @@
+# Run by typing "./newProj.js"
+
+touch index.html
+cp ~/Dropbox/bootCampHw/week-4-game/composer.json .
+cp ~/Dropbox/bootCampHw/week-4-game/index.php .
+mkdir assets
+mkdir assets/css assets/javascript assets/images assets/sounds
+cp ~/Dropbox/bootCampHw/week-4-game/assets/css/reset.css assets/css/
+touch assets/css/style.css assets/javascript/logic.js
+
