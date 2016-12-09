f5c08e20a6a8c62cb25f815bd2ae2bff73684cc3
ian shirley <ian shirley>
2016-12-08 18:55:28 -0600

    Firebase coding added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -15,8 +15,46 @@
   // Button for adding Trains
   $("#add-train-btn").on("click", function(){
 
+  	// This keeps the page from refreshing when the submit button is clicked
+  	event.preventDefault();
+
   	// This takes the user input
   	var trainName = $("#train-name-input").val().trim();
   	var trainDest = $("#destination-input").val().trim();
-  	var trainStart0 = $("#train-time-input").val().trim();
+  	var trainStart0 = moment($("#train-time-input").val().trim(), "HH:mm").format("X");
+  	var freq = $("#freq-input").val().trim();
+
+  	// This creates a lolcal temp object for the database
+  	var newTrain = {
+  		train: trainName,
+  		dest: trainDest,
+  		start0: trainStart0,
+  		frequency: freq
+  	}; // closes newTrain object
+
+  	// Uploads the data to the database
+  	database.ref().push(newTrain);
+
+  	// Alerts that a train has been added
+  	$("#myModal").modal();
+
+  	// Clears Textboxes
+  	$("#train-name-input").val("");
+	$("#destination-input").val("");
+	$("#train-time-input").val("");
+	$("#freq-input").val("");
+
+	// This keeps the page form refreshing if enter is hit
+	return false;
+  }); // This closes the add-train-btn listener
+
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  	// Stores everything into a variable
+  	var trainName = childSnapshot.val().train;
+	var trainDest = childSnapshot.val().dest;
+	var trainStart0 = childSnapshot.val().start0;
+	var freq = childSnapshot.val().freq;
+
+	// MATH
   })
